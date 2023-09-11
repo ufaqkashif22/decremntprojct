c8d25a459a5966fb0232f351a8a55e2e71a762e7
ufaq kashif <ufaqkashif22@gmail.com>
2023-09-11 23:20:55 +0400

    inc/dec prject

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const[incrmnt,setIncrmnt]=useState(0);
+
+    const first=()=>{
+        setIncrmnt(incrmnt+1)
+    }
+    const secnd=()=>{
+        if(incrmnt>0){
+        setIncrmnt(incrmnt-1)
+        }else{
+            alert("sorry!!! 0 is reached")
+            setIncrmnt(0)
+        }
+    }
+  return (
+    <>
+     <div className='mains_div'>
+     <div className='iner_div'>
+     <h1>{incrmnt}</h1>
+     <div className='btn_div'>
+        <button onClick={first}>Incremnt</button>
+        <button onClick={secnd}>Decremnt</button>
+     </div>
+
+     </div>
+
+     </div> 
+    </>
+  )
+}
+
+export default App;
+
