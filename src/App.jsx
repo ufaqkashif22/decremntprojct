25fd9a4f1a4cc0a9703c358e1ebf74071e581a68
ufaq kashif <ufaqkashif22@gmail.com>
2023-09-15 21:42:25 +0400

    new div add

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ const App = () => {
      <div className='btn_div'>
         <button onClick={first}>Incremnt</button>
         <button onClick={secnd}>Decremnt</button>
+        <div>ufaq</div>
      </div>
 
      </div>
